5adc5535a82ad26d0fef8139a6019af581cbb278
lucas-gab46 <121887128+lucas-gab46@users.noreply.github.com>
2024-11-14 19:44:43 -0300

    string.cpp

--- string.cpp
+++ string.cpp
@@ -5,17 +5,14 @@ int main(){
 // cin lẽ apenas palavras
 std::string str (6, 'A');
   str[6] = 'A';
-  std::cout << str[6];
   std::cout << ("String 1 Lida\n" ) << str << std::endl;
 
 std::string str2 (1 , 'B');
   str[1] = 'B';
-    std::cout << str[6];
     std::cout << ("String 2 Lida\n" ) << str2 << std::endl;
 
 
 std::string str3 (3, 'C');
-std::cout << str[3];
 std::cout << ("String 3 lida\n ") << str3 << std::endl;
 
 
